8385aa31df75a090a90473f25ba1151c68342bf2
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-04-06 17:43:10 +0200

    adattamento celle difficoltà

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,32 @@
 /*
 ---PROBLEMA---
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 */
 
-// COSTANTI
+// COSTANTI E VARIABILI
 const button = document.querySelector('button')
+
 const field = document.getElementById('campo')
+
+const root = document.querySelector(':root')
+
+let difficult = document.getElementById('selectDifficult')
+
+let num = randomNum(1, 100)
+
+// FUNZIONE NUMERO RANDOM
+function randomNum(min, max){
+    return Math.floor(Math.random()*max) + min;
+}
+
+// FUNZIONE ADATTAMENTO CELLA DIFFICOLTA'
+function adaptiveCell(x){
+    x = Math.sqrt(x)
+    root.style.setProperty('--granCelle', x) 
+}
 
 // EVENTO AL CLICK (BOTTONE)
 button.addEventListener('click', function() {
@@ -17,15 +35,25 @@ button.addEventListener('click', function() {
 
 // FUNZIONE AL CLICK (BOTTONE)
 function createField(){
-    for(let i = 1; i<=100; i++){
+    let selectDifficult = parseInt(difficult.value)
+
+    console.log(selectDifficult)
+
+    adaptiveCell(selectDifficult)
+
+    for(let i = 1; i<=selectDifficult; i++){
         let cubo = document.createElement('div')
+
         cubo.classList.add('cubo')
+
         cubo.innerText = i
+
         document.querySelector('#griglia').append(cubo)
 
         // EVENTO AL CLICK (CUBO)
         cubo.addEventListener('click', function() {
             colorCubo()
+
             console.log(this.innerText)
         })
 
